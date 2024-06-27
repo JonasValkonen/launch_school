74bdff133e991a10ae3f747522c87346693f7387
Jonas Valkonen <jonas.valkonen@gmail.com>
2024-06-27 18:58:30 +0800

    improvements on calculator

--- py101/lesson_2/calculator.py
+++ py101/lesson_2/calculator.py
@@ -8,6 +8,10 @@
     Returns:
     int or float: The sum of the two numbers.
 """
+LANGUAGE = 'en'
+import json
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
 
 def prompt(message):
     """
@@ -15,47 +19,65 @@ def prompt(message):
     """
     print(f"==> {message}")
 
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
+
 def invalid_number(number_str):
     """
     Checking the input for type
     """
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
     return False
 
-prompt('Welcome to calculator!')
+def calc():
+    """
+    Calculator main loop
+    """
 
-prompt(" What's the first number?")
-num1 = input()
-while invalid_number(num1):
-    prompt("Hmm, that doesn't look lika valid number.")
+    
+
+    prompt(" What's the first number?")
     num1 = input()
+    while invalid_number(num1):
+        prompt(messages('invalid_number', LANGUAGE))
+        num1 = input()
 
-prompt(" What's the second number?")
-num2 = input()
-while invalid_number(num2):
-    prompt("Hmm, that doesn't look lika valid number.")
+    prompt(" What's the second number?")
     num2 = input()
+    while invalid_number(num2):
+        prompt(messages('invalid_number', LANGUAGE))
+        num2 = input()
+    
+    input_min_len = min(len(num1),len(num2))
+    print(input_min_len)
 
-prompt("What operation would you like to perform?\n"
-      "1) Add 2) Subtract 3) Multiply 4) Divide")
-operation = input()
-
-while operation not in ["1", "2", "3", "4"]:
-    prompt('You must choose 1, 2, 3 or 4')
+    prompt("What operation would you like to perform?\n"
+        "1) Add 2) Subtract 3) Multiply 4) Divide")
     operation = input()
 
-match operation:
-    case '1':
-        output = int(num1) + int(num2)
-    case '2':
-        output = int(num1) - int(num2)
-    case '3':
-        output = int(num1) * int(num2)
-    case '4':
-        output = int(num1) / int(num2)
+    while operation not in ["1", "2", "3", "4"]:
+        prompt('You must choose 1, 2, 3 or 4')
+        operation = input()
+
+    match operation:
+        case '1':
+            output = float(num1) + float(num2)
+        case '2':
+            output = float(num1) - float(num2)
+        case '3':
+            output = float(num1) * float(num2)
+        case '4':
+            output = float(num1) / float(num2)
 
 
-prompt(f'The resutl is {output}')
+    prompt(f'The result is {output}')
+    prompt("""Do you want to do another calculation? y/n""")
+    reiterate = input()
+    if reiterate == 'y':
+        calc()
+
+prompt(messages('welcome',LANGUAGE))
+calc()
